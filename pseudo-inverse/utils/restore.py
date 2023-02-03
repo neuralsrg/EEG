3da8fc3803c4d0f19a056a2fd9dfdd81b4bd79ca
neuralsrg <jsetup@hotmail.com>
2023-02-03 19:05:42 +0300

    Added spectrum computation

---
+++ pseudo-inverse/utils/restore.py
@@ -0,0 +1,19 @@
+import numpy as np
+import librosa
+
+
+def restore(D, frame_size, hop_length, epochs=100, window='hann'):
+    
+    D = np.concatenate((np.zeros((D.shape[0], 1)), D, np.zeros((D.shape[0], 1))), axis=1)
+    mag, _ = librosa.magphase(D)
+    #mag = np.abs(D)
+    
+    phase = np.exp(1.j * np.random.uniform(0., 2*np.pi, size=mag.shape))
+    x_ = librosa.istft(mag * phase, hop_length=hop_length, center=False, window=window)
+    
+    for i in range(epochs):
+        _, phase = librosa.magphase(librosa.stft(x_, n_fft=frame_size, hop_length=hop_length, center=False,
+                                                 window=window))
+        x_ = librosa.istft(mag * phase, hop_length=hop_length, center=False, window=window)
+        
+    return x_[hop_length:-hop_length]
