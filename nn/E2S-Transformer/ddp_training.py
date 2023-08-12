ab4571896e71c606114b5c44e3593b4ea2999ccc
neuralsrg <jsetup@hotmail.com>
2023-08-12 16:09:46 +0300

    testing

--- nn/E2S-Transformer/ddp_training.py
+++ nn/E2S-Transformer/ddp_training.py
@@ -2,7 +2,8 @@ import os
 from tqdm import tqdm, trange
 
 import hydra
-from omegaconf import DictConfig
+from hydra.utils import instantiate
+from omegaconf import OmegaConf, DictConfig
 
 import torch
 import torch.nn.functional as F
@@ -14,8 +15,8 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import torch.distributed as dist
 from torch.distributed import init_process_group, destroy_process_group
 
-from data import ToyDataset, get_dl
-from model import get_model
+from data import get_dl
+from model import E2STransformer
 
 
 def ddp_setup(rank, world_size):
@@ -28,6 +29,39 @@ def ddp_setup(rank, world_size):
     os.environ["MASTER_PORT"] = "12355"
     init_process_group(backend="nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
+
+@hydra.main(version_base=None, config_path=".", config_name="config")
+def get_training_data(cfg: DictConfig):
+    # data
+    train_ds = instantiate(cfg.dataset)
+    val_ds = instantiate(cfg.dataset).set_val_mode(True)
+    train_dl, val_dl = get_dl(train_ds=train_ds, val_ds= val_ds, batch_size=cfg.training.batch_size)
+
+    # model 
+    model = E2STransformer(
+        n_channels=cfg.model.n_channels,
+        n_wvt_bins=cfg.model.n_wvt_bins,
+        d_model=cfg.model.d_model,
+        kernel_size=cfg.model.kernel_size,
+        emb_dropout=cfg.model.emb_dropout,
+        in_seq_len=cfg.model.in_seq_len,
+        n_fft=cfg.model.n_fft,
+        hop_size=cfg.model.hop_size,
+        nhead=cfg.model.nhead,
+        num_encoder_layers=cfg.model.num_encoder_layers,
+        num_decoder_layers=cfg.model.num_decoder_layers,
+        dim_feedforward=cfg.model.dim_feedforward,
+        dropout=cfg.model.dropout,
+        activation=cfg.model.activation,
+        audio_sr=cfg.model.audio_sr,
+        audio_paths=cfg.model.audio_paths,
+        eeg_sr=cfg.model.eeg_sr,
+        dj=cfg.model.dj,
+        example_input=train_ds[0][0]
+    )
+
+    print(model)
+    return train_dl, val_dl, model
 
 def train(rank, model, train_dl, criterion, optimizer):
 
@@ -67,13 +101,7 @@ def validate(rank, model, criterion, val_dl):
 
 def main(rank: int, world_size: int):
     ddp_setup(rank, world_size)
-    model = get_model()
-    train_dl = get_dl(ToyDataset())
-    val_dl = get_dl(ToyDataset())
-    criterion = torch.nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-    train(rank=rank, model=model, train_dl=train_dl, criterion=criterion, optimizer=optimizer)
-    validate(rank=rank, model=model, criterion=criterion, val_dl=val_dl)
+    train_dl, val_dl, model = get_training_data()
     destroy_process_group()
 
 
