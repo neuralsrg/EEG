005b17c0e1940bda52dc922b38d3edb510511c9c
neuralsrg <jsetup@hotmail.com>
2023-08-13 18:06:37 +0300

    testing

--- nn/E2S-Transformer/tqdm_test.py
+++ nn/E2S-Transformer/tqdm_test.py
@@ -4,6 +4,8 @@ import time
 for i1 in tqdm(range(100)):
     time.sleep(0.01)
     if (i1+1) % 50 == 0:
-        for i2 in tqdm(range(150)):
-            # do something, e.g. sleep
-            time.sleep(0.01)
+        with tqdm(total=100) as pbar:
+            for i2 in range(150):
+                # do something, e.g. sleep
+                time.sleep(0.01)
+                pbar.update(1)
