7918a4bd06d6eb70c35777ae44534d26c709e4fc
neuralsrg <jsetup@hotmail.com>
2023-08-13 19:57:41 +0300

    testing

--- nn/E2S-Transformer/tqdm_test.py
+++ nn/E2S-Transformer/tqdm_test.py
@@ -4,4 +4,5 @@ import random
 
 for i1 in (pbar := tqdm(range(100))):
     pbar.set_description(f'T|loss:{random.random():.2f}|best val:{random.random():.2f}|cur val:{random.random():.2f}')
+    pbar.set_postfix({'num_vowels': 78888888888881213})
     time.sleep(0.01)
