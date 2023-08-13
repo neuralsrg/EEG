eb6a12ced7a6d6b700e977a7d1207cb0f25f3991
neuralsrg <jsetup@hotmail.com>
2023-08-13 19:39:51 +0300

    testing

--- nn/E2S-Transformer/tqdm_test.py
+++ nn/E2S-Transformer/tqdm_test.py
@@ -2,6 +2,6 @@ from tqdm import tqdm, trange
 import time
 import random
 
-for i1 in (pbar := tqdm(range(100), bar_format="{desc:<50}{percentage:<20}%|{bar:<20}{r_bar:<20}")):
+for i1 in (pbar := tqdm(range(100), bar_format="{desc:<50}{percentage:3.0f}%|{bar:10}{r_bar}")):
     pbar.set_description(f'T|loss:{random.random():.2f}|best val:{random.random():.2f}|cur val:{random.random():.2f}')
     time.sleep(0.01)
