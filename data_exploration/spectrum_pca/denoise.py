3fb00fa05e37ec5e73d3d0b9ff3e94a417b39b0e
neuralsrg <jsetup@hotmail.com>
2024-02-09 00:51:17 +0300

    Add denoising sensitivity

--- data_exploration/spectrum_pca/denoise.py
+++ data_exploration/spectrum_pca/denoise.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Denoises audio files.')
     parser.add_argument('-d', '--dir', type=str, help='Directory with audio files')
     parser.add_argument('-o', '--output_dir', type=str, help='Directory for denoised audios', default='denoised_audios')
+    parser.add_argument('-s', '--sens', type=float, help='Denoising sensitivity', default=1.0)
     args = parser.parse_args()
 
     if not os.path.exists(args.output_dir):
@@ -19,7 +20,7 @@ if __name__ == '__main__':
 
     for filename in files:
         rate, data = wavfile.read(filename)
-        reduced_noise = nr.reduce_noise(y=data, sr=rate)
+        reduced_noise = nr.reduce_noise(y=data, sr=rate, stationary=False, prop_decrease=args.sens)
         _, output_file = os.path.split(filename)
         output_file = os.path.join(args.output_dir, output_file)
         wavfile.write(output_file, rate, reduced_noise)
